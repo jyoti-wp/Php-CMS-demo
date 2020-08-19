8aa31e13cc46817454e4e15014bf5ba779267036
Jyoti Shukla <jyoti@Jyotis-MacBook-Air.local>
2020-08-19 10:50:13 +0530

    comment section php update

---
+++ embed.php
@@ -0,0 +1,10 @@
+<?php ?>
+<div class="demo"></div>
+    <ul>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+        <li><a href="">home</a></li>
+    </ul>
+</div>
